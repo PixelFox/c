4c948a5ca639d9ce4eb1113de38dc23ed1b39cb4
Jay Franklin <john.jay.franklin@gmail.com>
2012-11-26 18:17:22 -0500

    added cs263 stuffs

---
+++ cs263/lesson2/lesson2.c
@@ -0,0 +1,42 @@
+/********************************************/
+/*     Volume of a rectangle                */
+/* Author:  Jay Franklin                    */
+/* Created: 20121126                        */
+/* Version: 1.2                             */
+/*                                          */
+/********************************************/
+/* Changelog:                               */
+/*  1.2 - 'prettied up' output              */
+/*  1.1 - scanf incorrect                   */
+/*  1.0 - initial version                   */
+/*                                          */
+/********************************************/
+/*                                          */
+/*  Take input of 3 sides of rectangle and  */ 
+/*   compute the volume. Output to user.    */
+/*                                          */
+/*  volume = length * width * height        */
+/*                                          */
+/********************************************/
+
+#include <stdio.h>    // required for input/output
+
+int main() {	     // begin main function
+
+	int length, width, height;  // initialize three required variables
+
+	// interact with user
+	printf("Please input the length, width, and height of rectangle seperated by spaces followed by <enter> or <return>.\n");
+	// scanf("%d",&length,"%d",&width,"%d",&length);   <-- incorrect. 
+	scanf("%d %d %d",&length,&width,&height);
+	
+	// Compute the volume
+	int volume;
+	volume = length * width * height;
+
+	// Output the results
+	printf("\n\n\nProvided Values:\n\tlength:\t%d\n\twidth:\t%d\n\tlength:\t%d\n\nComputed Results:\n\tVolume:\t%d\n\n",length,width,height,volume);
+
+	return 0;
+}
+
